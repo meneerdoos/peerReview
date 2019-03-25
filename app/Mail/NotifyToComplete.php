e8909f7ce6c885835249bb2410c4e653ae6799f1
mrdoos <meneerdooz@gmail.com>
2019-03-25 15:28:49 +0100

    message

---
+++ app/Mail/NotifyToComplete.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NotifyToComplete extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $link ;
+    public $id;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct( $link, $id )
+    {
+        $this->link = $link ;
+        $this->id = $id ;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('notify.toComplete');
+    }
+}
